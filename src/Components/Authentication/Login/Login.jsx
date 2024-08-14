4e24551a588cdfd1af0eab7efd2e2937d51343bb
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-08-14 22:23:50 +0600

    auth provider and firebase added

--- src/Components/Authentication/Login/Login.jsx
+++ src/Components/Authentication/Login/Login.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+// import { Bounce, JackInTheBox, Slide, Zoom } from "react-awesome-reveal";
+
+import { Bounce, JackInTheBox, Slide, Zoom } from "react-awesome-reveal";
+
 
 const Login = () => {
     return (
-        <div>
-            login
+        <div className="md:w-1/2 w-3/4 mx-auto my-3" >
+            <JackInTheBox>
+                <form onSubmit={handleLogin} className="bg-blue-200 p-10  rounded-2xl">
+                    <Slide delay={500}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name="email" className="input input-bordered" />
+                        </div>
+                    </Slide>
+                    <Slide delay={800} direction="right">
+                        <div className="form-control relative">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type={show ? 'text' : 'password'} placeholder="password" name="password" className="input input-bordered" />
+                            <span className="absolute top-[52px] right-3" onClick={() => setShow(!show)}>
+                                {show ?
+                                    <FaEye /> :
+                                    <FaEyeSlash />
+                                }
+                            </span>
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                    </Slide>
+                    <Slide delay={1100}>
+                        <div className="form-control my-3">
+                            <button className="btn bg-[#adf010]">Login</button>
+                        </div>
+                    </Slide>
+                    <Slide delay={1500} direction="right">
+                        <p>Do not Have An Account? <Link className="underline" to="/register">Register One</Link></p>
+                    </Slide>
+                </form>
+            </JackInTheBox>
+            <Zoom delay={1700}>
+                <div className="divider my-10">Or</div>
+            </Zoom>
+            <Bounce delay={2000}>
+                <div onClick={handleGoogleLogin} className="flex items-center gap-3 btn text-xl my-2">
+                    <FaGoogle size={20} />
+                    <span>Google Login</span>
+                </div>
+            </Bounce>
+            <Bounce delay={1000}>
+                <div onClick={handleGithubLogin} className="flex items-center gap-3 btn text-xl my-2">
+                    <FaGithub size={20} />
+                    <span>Github Login</span>
+                </div>
+            </Bounce>
         </div>
     );
 };
---
+++ src/Contexts/AuthProvider.jsx
@@ -0,0 +1,57 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { createContext, useEffect, useState } from "react";
+
+
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+    // GOOGLE LOGIN 
+    const googleProvider = new GoogleAuthProvider();
+    const googleLogin = () => {
+        setIsLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+    // login with github
+    // const githubProvider = new GithubAuthProvider();
+    // const githubLogin = () => {
+    //     setIsLoading(true);
+    //     return signInWithPopup(auth, githubProvider)
+    // }
+    // sign in user with email and password
+    const signInUser = (email, password) => {
+        setIsLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    };
+    // log out
+    const logOut = () => {
+        setIsLoading(true);
+        return signOut(auth)
+    };
+    // observe the user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setIsLoading(false);
+        });
+        return () => {
+            unsubscribe();
+        }
+    }), [];
+    const userInfo = { user, setUser, isLoading, createUser, googleLogin, githubLogin, signInUser, logOut }
+
+    return (
+        <AuthContext.Provider value={userInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export default AuthProvider;
